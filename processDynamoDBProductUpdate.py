8997a5d7793098fcd4ef8f06dc8d14a26f9e931b
Roman Kovalchuk <kovalchuk.roman03@gmail.com>
2022-06-22 00:03:18 +0300

    added post_handler, implemented putting item in DynamoDB Product table

--- processDynamoDBProductUpdate.py
+++ processDynamoDBProductUpdate.py
@@ -1,37 +1,90 @@
 import json
+import boto3
+
+client = boto3.client('dynamodb')
 
 
-def lambda_handler(event, context):
-    print('DynamoDB product update!')
-    records = event['Records']
-    print_sep("-", 20)
-    print(f'Number of records:{len(records)}')
-    print_sep("-", 20)
-
-    for record in records:
-        print_record(record)
-        print_sep("-", 20)
-
+def get_handler(event, context):
+    req_context = event["requestContext"]
+    http = req_context["http"]
+    print("get_handler invoked!")
+    path = http["path"]
+    protocol = http["protocol"]
+    print(f"path: {path}")
+    print(f"protocol: {protocol}")
     return {
         'statusCode': 200,
-        'body': json.dumps('DynamoDBProductUpdate')
+        'body': "method GET isn't properly implemented yet."
     }
 
 
-def print_sep(symb, repeat):
-    print(symb * repeat)
+def add_attribute(data_item, key, db_type, value):
+    if value is not None:
+        data_item[key] = {f'{db_type}': f'{value}'}
 
 
-def resolve_item_name(event_name):
-    if event_name == "REMOVE":
-        return "OldImage"
-    elif event_name == "INSERT":
-        return "NewImage"
+def is_item_present(key_item):
+    response = client.get_item(
+        TableName='Product',
+        Key=key_item
+    )
+    print('response')
+    print(response)
+    item = response.get('Item', None)
+    return True if item is not None else False
 
 
-def print_record(db_record):
-    dynamo = db_record['dynamodb']
-    print(f"{db_record['eventName']}")
-    item_name = resolve_item_name(db_record['eventName'])
-    item = {**dynamo['Keys'], **dynamo[item_name]}
-    print(f'item: {item}')
+def post_handler(event, context):
+    print("post_handler invoked")
+    body = json.loads(event["body"])
+    product_id = body['product_id']
+    name = body.get('name', None)
+    picture_id = body.get('picture_id', None)
+    price = body.get('price', None)
+    data_item = {
+        'product_id': {
+            'N': f'{product_id}'
+        },
+        'name': {
+            'S': f'{name}'
+        }
+    }
+
+    if is_item_present(data_item):
+        return {
+            'statusCode': 400,
+            'body': f'Item with partition id \'{product_id}\' and sort id \'{name}\' already exists!'
+        }
+
+    add_attribute(data_item, 'picture_id', 'S', picture_id)
+    add_attribute(data_item, 'price', 'N', price)
+
+    data = client.put_item(
+        TableName='Product',
+        Item=data_item
+    )
+    print(f"product_id: {product_id}")
+    print(f"name: {name}")
+
+    print("data: ")
+    print(data)
+    response_body = 'Successfully added item to DynamoDB table Product!\n' + json.dumps(data_item)
+    return {
+        'statusCode': 200,
+        'body': response_body
+    }
+
+
+dispatch = {
+    "GET": get_handler,
+    "POST": post_handler
+}
+
+
+def lambda_handler(event, context):
+    method = event["requestContext"]["http"]["method"]
+    response = dispatch[method](event, context)
+
+    print(f"event: {event}")
+
+    return response
